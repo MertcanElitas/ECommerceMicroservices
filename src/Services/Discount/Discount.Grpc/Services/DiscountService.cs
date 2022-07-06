e04c7e62902d0ef7ebcd7ed3736f4b5c97cd0f27
melitas <mertcan.elitas@hepsiburada.com>
2022-07-06 20:56:46 +0300

    feat: grpc dev

---
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,15 @@
+﻿using Grpc.Core;
+using Discount.Grpc;
+
+namespace Discount.Grpc.Services;
+
+public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+{
+    private readonly ILogger<DiscountService> _logger;
+    public DiscountService(ILogger<DiscountService> logger)
+    {
+        _logger = logger;
+    }
+    
+}
+
